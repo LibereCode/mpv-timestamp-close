d79d841bed71f0874e308e8e22861deaec7e712d
LibereCode <libere-code@posteo.org>
2026-09-03 14:38:30 +0200

    feat: Script now targets ~/.local/state/mpv/timestamp-close.json

--- timestamp-close.lua
+++ timestamp-close.lua
@@ -1,6 +1,6 @@
 ---Copyright (c) 2026 LibereCode. All Rights Reserved.
 ---
----Version: 0.0.2
+---Version: 0.0.3
 
 -- show the name of current playing file
 -- press SHIFT+ENTER to call the function
@@ -9,50 +9,16 @@ local mp = require("mp")
 local utils = require("mp.utils")
 local options = require("mp.options")
 
---[[
-    INFO:
-    On close (or before it)
-    Update a file with a (JSON ?) table of values,
-    with the path to media and timestamp at exit.
+local M = { time = {}, state = {} }
 
-    EXAMPLE:
-    ```json
-    {
-        "/path/to/video.mp4": 1234.723
-        "/another/path/to/vid.mp4": 111.111
-    }
-    ```
-    Where the number is the amount of time in seconds.
+---INFO Creating path to state(json) file
 
-    tasks/features:
-    - Async
-    - Read file with json-parser.
-    - Set time if `json_parsed[path]` have a value (~= nil).
-    - On exit-event, use:
-        ```lua
-        json_parsed[path] = timestamp;
-        local new_json_str = lua_2_json(json_parsed)
-        io.open(path/state.json, "w"):write(new_json_str)
-        ```
---]]
-
---[[
-    Events <https://rksvc.github.io/mpv-manual/command-interface/list-of-events/>
-    ------
-    - start-file (MPV_EVENT_START_FILE)
-    - end-file (MPV_EVENT_END_FILE)
-        Alternative do the action before quit command ?
---]]
-
---- INFO REFERENCE
--- function on_pause_change(name, value)
---     if value == true then
---         mp.set_property("fullscreen", "no")
---     end
--- end
--- mp.observe_property("pause", "bool", on_pause_change)
-
-local M = { time = {} }
+M.state.dir = (os.getenv("XDG_STATE_HOME") or (os.getenv("HOME") .. "/.local/state")) .. "/mpv"
+M.state.file = M.state.dir .. "/timestamp-close.json"
+local dir_check = utils.file_info(M.state.dir)
+if not (dir_check or {}).is_dir then
+    os.execute("mkdir " .. M.state.dir)
+end
 
 ---Read a file from `filepath`, and return content as a string
 ---@param filepath string
@@ -85,8 +51,8 @@ end
 
 ---Initial time
 mp.register_event("file-loaded", function()
-    M.json_path = "/tmp/mpv-timestamp-close.json"
-    os.execute("touch " .. M.json_path) -- I am lazy
+    M.json_path = M.state.file -- "/tmp/mpv-timestamp-close.json"
+    os.execute("touch " .. M.state.file) -- I am lazy
     M.json_tbl = M.json2lua(M.readFile(M.json_path)) or {}
     M.path = mp.get_property_native("path") or "idk"
     mp.commandv("seek", M.json_tbl[M.path] or 0)
